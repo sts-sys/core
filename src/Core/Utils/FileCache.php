e9a8cfd4cfaf2dc82a77c740353c5dfdfb856d2a
sts-sys <sts.framework@gmail.com>
2024-09-07 14:52:40 +0200

    Update FileCache.php

--- src/Core/Utils/FileCache.php
+++ src/Core/Utils/FileCache.php
@@ -13,7 +13,6 @@ class FileCache
         $this->loadCache();
     }
 
-    // Încarcă datele cache-ului din fișier
     protected function loadCache(): void
     {
         if (file_exists($this->cacheFile)) {
@@ -21,19 +20,16 @@ class FileCache
         }
     }
 
-    // Salvează datele cache-ului în fișier
     public function saveCache(): void
     {
         file_put_contents($this->cacheFile, '<?php return ' . var_export($this->cacheData, true) . ';');
     }
 
-    // Obține o valoare din cache
     public function get(string $key)
     {
         return $this->cacheData[$key] ?? null;
     }
 
-    // Setează o valoare în cache
     public function set(string $key, $value): void
     {
         $this->cacheData[$key] = $value;
