0c8eb28cc3b6e8a5987c9246a4d62d90418fdb79
sts-sys <sts.framework@gmail.com>
2024-09-07 15:19:49 +0200

    Create Application.php

---
+++ src/Core/Application.php
@@ -0,0 +1,34 @@
+<?php
+namespace MyApp\Core;
+
+use STS\Core\Containers\ServiceContainer;
+
+class Application
+{
+    protected $serviceContainer;
+
+    public function __construct()
+    {
+        $this->serviceContainer = new ServiceContainer();
+        $this->registerBaseServices();
+    }
+
+    protected function registerBaseServices(): void
+    {
+        // Înregistrează servicii de bază în container
+        $this->serviceContainer->bind('request', function () {
+            return new \MyApp\Core\Http\Request();
+        });
+
+        $this->serviceContainer->bind('response', function () {
+            return new \MyApp\Core\Http\Response();
+        });
+
+        // Înregistrează alți furnizori de servicii, etc.
+    }
+
+    public function getServiceContainer(): ServiceContainer
+    {
+        return $this->serviceContainer;
+    }
+}
