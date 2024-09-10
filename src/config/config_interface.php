80aca85ca094fc5d4dd5362f5f185d1419eb6425
Costin Ionuț <sts.framework@gmail.com>
2024-09-10 17:24:57 +0200

    Update config_interface.php

--- src/config/config_interface.php
+++ src/config/config_interface.php
@@ -3,7 +3,7 @@ namespace sts\config;
 
 interface config_interface
 {
-    public function get(string $config, string $key = null): array|null;
+    public function get(string $config, string $key = null): string|null;
     public function set(string $config, string $key = null, ?string &$value = ''): void;
     public function delete(string $config, string $key = null): void;
     public function all(): array;
